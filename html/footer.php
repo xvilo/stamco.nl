79b250aa9dbf10ef409d9cea0d44d5c34c96f5a1
Sem Schilder <me@sem.design>
2016-08-22 09:09:40 +0200

    small changes

--- html/footer.php
+++ html/footer.php
@@ -1,7 +1,7 @@
 		<footer class="page-footer">
 			<div class="container">
-				<div class="page-footer--logo"><i class="m-icon icon--ui__stamco_logo_big"><svg><use xlink:href="<?php echo get_template_directory_uri() ?>/media/images/sprites/ui.svg#stamco_logo_big" xmlns:xlink="http://www.w3.org/1999/xlink"></use></svg></i></div>
-				<div class="page-footer--logo__clear"><i class="m-icon icon--ui__stamco_logo_clear"><svg><use xlink:href="<?php echo get_template_directory_uri() ?>/media/images/sprites/ui.svg#stamco_logo_clear" xmlns:xlink="http://www.w3.org/1999/xlink"></use></svg></i></div>
+				<div class="page-footer--logo"><a href="<?php echo site_url(); ?>"><i class="m-icon icon--ui__stamco_logo_big"><svg><use xlink:href="<?php echo get_template_directory_uri() ?>/media/images/sprites/ui.svg#stamco_logo_big" xmlns:xlink="http://www.w3.org/1999/xlink"></use></svg></i></a></div>
+				<div class="page-footer--logo__clear"><a href="<?php echo site_url(); ?>"><i class="m-icon icon--ui__stamco_logo_clear"><svg><use xlink:href="<?php echo get_template_directory_uri() ?>/media/images/sprites/ui.svg#stamco_logo_clear" xmlns:xlink="http://www.w3.org/1999/xlink"></use></svg></i></a></div>
 				<?php if ( is_active_sidebar( 'footer' ) ) : ?>
 					<div class="page-footer--menu">
 						<?php dynamic_sidebar( 'footer' ); ?>
