82d7304d25d0587506c1e10659d43605fd07e403
Sem Schilder <me@sem.design>
2016-09-02 14:47:35 +0200

    getting rid of annoying version numbers1

--- html/functions.php
+++ html/functions.php
@@ -132,3 +132,10 @@ function add_theme_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
 
+function remove_cssjs_ver( $src ) {
+    if( strpos( $src, '?ver=' ) )
+        $src = remove_query_arg( 'ver', $src );
+    return $src;
+}
+add_filter( 'style_loader_src', 'remove_cssjs_ver', 10, 2 );
+add_filter( 'script_loader_src', 'remove_cssjs_ver', 10, 2 );
