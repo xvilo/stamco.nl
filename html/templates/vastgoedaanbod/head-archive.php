c25e00af10614666b6c727601822bb68a26b3f0c
Sem Schilder <me@sem.design>
2016-09-02 14:42:12 +0200

    Archive image fix

--- html/templates/vastgoedaanbod/head-archive.php
+++ html/templates/vastgoedaanbod/head-archive.php
@@ -1,4 +1,4 @@
-			<section class="page-header">
+			<section class="page-header__image" style="background-image: url('/wp-content/uploads/2016/08/Sky.jpg')">
 				<div class="container">
 					<h1 class="page-header--title"><?php echo post_type_archive_title() ?></h1>
 				</div>
